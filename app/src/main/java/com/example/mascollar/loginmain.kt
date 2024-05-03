7d22991c684a9a1821c91f9ea377758843933450
ElCarlex27 <carlosocampo2711@gmail.com>
2024-05-02 22:51:54 -0600

    compose

--- app/src/main/java/com/example/mascollar/loginmain.kt
+++ app/src/main/java/com/example/mascollar/loginmain.kt
@@ -5,8 +5,25 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mascollar.ui.theme.MascollarTheme
@@ -36,10 +53,50 @@ class loginmain : AppCompatActivity() {
 
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MascollarTheme {
-
+        buttonLogin()
     }
 }
+
+
+@Composable
+fun buttonLogin() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Red)
+    ) {
+
+        Text(
+            text = "Login",
+            fontSize = 30.sp,
+            color = Color.White,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            painter = painterResource(id = R.drawable.iconmain),
+            contentDescription = "Logo mascollar",
+
+            )
+        Text(text = "Usuario", color = Color.White)
+        Text(text = "Contraseña", color = Color.White)
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(text = "Usuario", color = Color.White)
+            Text(text = "Contraseña", color = Color.White)
+        }
+    }
+}
+
